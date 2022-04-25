c2c9e4be1103aa73aa12b0df5ec718086c02b4cb
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-25 14:53:33 -0600

    working through some concepts
    Working through concepts on objects, will return to finish

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//Buisness Logic 
+//Task constructor 
+let taskList = [];
+
+function Task(description,priority,assignee,complete) {
+  this.description = description;
+  this.priority = priority;
+  this.assignee = assignee;
+  this.complete = complete;
+}
+//UI Logic 
+$(document).ready(function(){
+  $("#userInput").submit(function(event){
+  const description = $("input#description").val();
+  const priority = $("input:radio[name=priority]:checked").val();
+  const assignee = $("input#assignee").val();
+  const complete = false;
+  
+  let newTask = new Task(description,priority,assignee,complete);
+  taskList.push(newTask);
+
+  taskList.sort(function(a,b){
+    return a.priority - b.priority;
+  });
+
+  $("#toDoList").text(`Task: ${taskList}`);
+  
+  
+
+  console.log(taskList);
+  event.preventDefault();
+  })
+});
