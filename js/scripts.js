7bbf84ea9fb5f10224814b1e584bd5ea17cad1c6
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-04-30 09:51:47 -0600

    Object.keys work
    add text to script.js file to iterate and print through the tasks properties of the taskList objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,63 @@
 //Buisness Logic 
-let taskList = [];
+//Task List constructor 
+function TaskList() {
+  this.tasks = {};
+}
+
+TaskList.prototype.addTask = function(task) {
+  this.tasks[task.title] = task;
+};
+
+
 //Task constructor 
-function Task(description,priority,assignee,complete) {
+function Task(title,description,priority,assignee,complete) {
+  this.title = title;
   this.description = description;
   this.priority = priority;
   this.assignee = assignee;
   this.complete = complete;
 }
+
 //UI Logic 
+let taskList = new TaskList;
+  
+function displayTasksList(taskListToDisplay){
+  let taskList = $("ol#toDoList");
+  let htmlForList = "";
+
+  Object.keys(taskListToDisplay.tasks).forEach(function(key) {
+    const task = taskListToDisplay.key;
+    htmlForList += "<li>" + task[key] +"</li>";
+  });
+  taskList.html(htmlForList);
+
+  $("ol#toDoList").show();
+}
 
 $(document).ready(function(){
-
-  
   $("#userInput").submit(function(event){
     event.preventDefault();
+    const title = $("input#title").val();
     const description = $("input#description").val();
     const priority = $("input:radio[name=priority]:checked").val();
     const assignee = $("input#assignee").val();
     const complete = false;
   
-    let newTask = new Task(description,priority,assignee,complete);
-    taskList.push(newTask);
-
+    let newTask = new Task(title,description,priority,assignee,complete);
+    
+    taskList.addTask(newTask);
+    
+    /*
     taskList.sort(function(a,b){
-      return a.priority - b.priority;
+      return a.tasks.priority - b.tasks.priority;
     });
 
-  $("#toDoList").text(`Task: ${taskList}`);
-  
-  
+    */
 
-  console.log(taskList);
+
+    console.log(taskList.tasks);
+    displayTasksList(taskList);
+    
+
   })
 });
